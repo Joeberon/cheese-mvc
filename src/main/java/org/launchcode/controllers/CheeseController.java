890c57a6b890245975dd223a111cc2ea50ef84e8
John Doe <kercherjoe@gmail.com>
2018-01-08 19:08:22 -0600

    Final

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -1,8 +1,10 @@
-package org.launchcode.controllers;
+package org.launchcode.cheesemvc.controllers;
 
-import org.launchcode.models.Cheese;
-import org.launchcode.models.CheeseData;
-import org.launchcode.models.CheeseType;
+import org.launchcode.cheesemvc.models.Category;
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.data.CategoryDao;
+import org.launchcode.cheesemvc.models.data.CheeseDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,20 +14,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 
-/**
- * Created by LaunchCode
- */
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    // Request path: /cheese
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    // /Cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("cheeses",cheeseDao.findAll());
         model.addAttribute("title", "My Cheeses");
 
         return "cheese/index";
@@ -35,38 +39,49 @@ public class CheeseController {
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
         model.addAttribute(new Cheese());
-        model.addAttribute("cheeseTypes", CheeseType.values());
+        model.addAttribute("categories", categoryDao.findAll());
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese,
-                                       Errors errors, Model model) {
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese,
+                                       Errors errors, @RequestParam int categoryId,
+                                       Model model) {
 
-        if (errors.hasErrors()) {
+        if (errors.hasErrors()){
             model.addAttribute("title", "Add Cheese");
+            model.addAttribute("categories", categoryDao.findAll());
             return "cheese/add";
         }
-
-        CheeseData.add(newCheese);
+        Category cat = categoryDao.findOne(categoryId);
+        newCheese.setCategory(cat);
+        cheeseDao.save(newCheese);
         return "redirect:";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", CheeseData.getAll());
-        model.addAttribute("title", "Remove Cheese");
+        model.addAttribute("cheeses", cheeseDao.findAll());
+        model.addAttribute("title", "Remove Cheeses");
         return "cheese/remove";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
-
-        for (int cheeseId : cheeseIds) {
-            CheeseData.remove(cheeseId);
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds){
+        for(int cheeseId : cheeseIds){
+            cheeseDao.delete(cheeseId);
         }
-
         return "redirect:";
     }
+   /* Edit Starts here
+   @RequestMapping(value ="edit", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @RequestParam int cheeseIds){
+        for(int cheeseId : cheeseIds){
+            CheeseData.getById(cheeseId);
+    }
+    @RequestMapping(value = "edit", method = RequestMethod.POST)
+    public String processEditForm(int cheeseId, String name, String description){
+    } */
+
 
 }
---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,48 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.launchcode.cheesemvc.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("user");
+        return "user/index";
+    }
+
+    @RequestMapping(value = "add")
+    public String displayAddForm(Model model, @ModelAttribute User user) {
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddForm(@ModelAttribute @Valid User user,
+                                 Errors errors, Model model, String verify) {
+        System.out.println("abc " + verify + " " + user);
+
+        if (errors.hasErrors()){
+            model.addAttribute(user);
+            return "user/add";
+        }
+
+
+        if (user.getPassword().equals(verify)){
+            model.addAttribute(user);
+            return "user/index";
+        }else {model.addAttribute("verify", "Password and Verify must match");
+            return "user/add";
+        }
+
+    }
+}
